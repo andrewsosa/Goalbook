b630605f4913153bfb08daebb18123d1bb37a8dd
Andrew Sosa <sosa@cs.fsu.edu>
2015-11-09 15:43:53 -0500

    Removed Parse Library files

--- app/src/main/java/com/andrewsosa/bounce/Bounce.java
+++ app/src/main/java/com/andrewsosa/bounce/Bounce.java
@@ -1,14 +1,8 @@
 package com.andrewsosa.bounce;
 
 import android.app.Application;
-
 import com.firebase.client.Firebase;
-import com.parse.Parse;
-import com.parse.ParseObject;
 
-/**
- * Created by andrewsosa on 4/6/15.
- */
 public class Bounce extends Application {
 
     public static final String URL = "https://bouncetodo.firebaseio.com";
@@ -17,11 +11,6 @@ public class Bounce extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Parse.enableLocalDatastore(getApplicationContext());
-        Parse.initialize(this, "tdorw6A41hVhwXtMn1Pe07jfPXQOZyJEP6ztLNAX", "5eNVvRRhSJUaQ895fhN4vy2C3SjuPBQ4wv5KzAMl");
-
-        ParseObject.registerSubclass(ParseTask.class);
-        ParseObject.registerSubclass(ParseTaskList.class);
 
         Firebase.setAndroidContext(this);
         Firebase.getDefaultConfig().setPersistenceEnabled(true);
